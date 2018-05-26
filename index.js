041bc2abafdfa1fcd679b9fea1dac0d2154571e3
eitan <eitan.burke@gmail.com>
2018-05-26 19:21:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
+"hello, ${specialguest}! high ${3+2}!"
 
 var topic = "space";
 
